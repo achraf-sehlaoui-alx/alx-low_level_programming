d78918939a08aa8284e4f809fe9d45edab95a17b
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-09-07 08:41:54 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a C progam that print with puts fonction
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
