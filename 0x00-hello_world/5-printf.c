cfca707aae2906c35919cdb929937853d137c223
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-09-07 08:51:29 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - start pt
+ *
+ * Description: a C program that print with the printf fonction
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
