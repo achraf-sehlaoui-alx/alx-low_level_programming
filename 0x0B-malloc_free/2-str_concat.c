e2058744cc14bf77cf30e3fdbe6434e3c2db9a6f
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-04 17:21:05 +0000

    2-fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,34 +18,25 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	i = ci = 0;
-	
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
 		ci++;
-	
 	conct = malloc(sizeof(char) * (i + ci + 1));
-
 	if (conct == NULL)
 		return (NULL);
-	
 	i = ci = 0;
-	
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
 		i++;
 	}
-
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
-		i++;
-		ci++;
+		i++, ci++;
 	}
 	conct[i] = '\0';
-	
 	return (conct);
 }
