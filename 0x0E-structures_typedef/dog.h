030d99d6066263c8c3a8757d6688cdce7981655f
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-10 22:28:23 +0000

    one

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: Lone dog struct in a big kitty world
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
