c2a5abb74f3044b412bd2cf9ea555213c8fe50d3
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-11 21:48:43 +0000

    fixx

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,11 +23,8 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (s && s[0] == ops[i].op[0] && !s[1])
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
 }
