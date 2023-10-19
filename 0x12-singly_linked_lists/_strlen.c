9a114f162bfaf9d931dc864b97d8b0d54853b462
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-19 14:32:04 +0000

    done

---
+++ 0x12-singly_linked_lists/_strlen.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * int _strlen - returns the length of a string
+ * @s: the string whose length to check
+ * Return: integer length of string
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
