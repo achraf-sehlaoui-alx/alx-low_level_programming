a2c70a65e39a03ea3ee9e551d5ed6d9945813a3e
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-24 15:21:16 +0000

    task 3 fixt

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,14 +18,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL)
+	if (!*head)
 	{
 		*head = new;
 		return (new);
 	}
 
 	while (t->next)
-		t = temp->next;
+		t = t->next;
 
 	t->next = new;
 
