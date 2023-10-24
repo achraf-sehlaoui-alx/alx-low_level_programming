3072f352aed9d20f92cdc7b8b36c1e0fc3b90caa
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-24 16:21:36 +0000

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a list
+ * @head: pointer to the first node
+ *
+ * Return: the first node in the new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev_node = NULL;
+	listint_t *next_node = NULL;
+
+	if (!head || !*head)
+		return (NULL);
+
+	while (*head)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prev_node;
+		prev_node = *head;
+		*head = next_node;
+	}
+
+	*head = prev_node;
+
+	return (*head);
+}
