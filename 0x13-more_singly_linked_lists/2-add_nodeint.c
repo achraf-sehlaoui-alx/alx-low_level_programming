e999ba3697a60e8b1af2e4405ef38f630c493c7e
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-24 15:12:50 +0000

    task 2 fixt wrt

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,14 +11,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node = malloc(sizeof(listint_t));
 
-	if (!new_node)
+	if (!new_node || !head)
 		return (NULL);
 
 	new_node->next = NULL;
 	new_node->n = n;
 	if (*head)
 		new_node->next = *head;
-	*head = new_noed;
+	*head = new_node;
 
 	return (new_node);
 }
