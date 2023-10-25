76d79cccb88b8512aba8b451e807c58a8fcc29a7
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-25 21:10:25 +0000

    .

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - prints a linked list
+ * @h: pointer to the list to print
+ *
+ * Return: the size of the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
