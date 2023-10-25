828a6a2bc2154a96389af422b5fdee816fe7e53b
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-25 22:35:43 +0000

    fin byt

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,9 @@
-section 	.text
-	global	main
-	extern	printf
-maim:
-	mov	edi, format
-	mov	eax, 0
-	call	printf
-
-section		.data
-	format db 'Hello, Holberton', 0xa, 0
+       global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
