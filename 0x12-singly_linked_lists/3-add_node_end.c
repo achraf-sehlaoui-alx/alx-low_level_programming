927744cc0d0a0db9a853b22b3371011ab668a76b
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-25 21:56:27 +0000

    t

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add a node at the end of the list
+ * @head: the head of the list
+ * @str: string to put in the new node
+ *
+ * Return: the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	list_t *t = *head;
+	unsigned int size = 0;
+
+	while (str[size])
+		size++;
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = size;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+ 	}
+
+	while (t->next)
+		t = t->next;
+	t->next = new;
+	return (new);
+}
