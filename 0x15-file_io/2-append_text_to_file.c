f99bbde0ceb3f45277772436c5d6379f96147e4f
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-30 23:14:01 +0000

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at file
+ * @filename: name of the file to create
+ * @text_content: content to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (!filename)
+		return (-1);
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
+}
