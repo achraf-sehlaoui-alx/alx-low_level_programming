d20361f5618abc28fdc93f52e67bb6670a4a88be
achraf-sehlaoui-alx <sehlaoui.achraf@gmail.com>
2023-10-30 23:22:11 +0000

    task 1 .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,15 +15,20 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[n] != '\0')
-		n++;
+	if (!text_content)
+		text_content = "";
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_TRUSR | S_IWUSR);
+	while (text_content[n] != '\0')
+	{
+		n++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file == -1)
 		return (-1);
 
-	write(file, text_content, i);
+	write(file, text_content, n);
 
 	return (1);
 }
